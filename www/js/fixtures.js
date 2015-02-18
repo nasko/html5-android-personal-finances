8d7b2c9a7cc143a8e85e13059d8654438719e34d
Todor Mitovski <harvaru@gmail.com>
2015-02-18 03:27:34 +0200

    Trying to remove conflicts

---
+++ www/js/fixtures.js
@@ -0,0 +1,16 @@
+var transactions = {
+    "2015": {
+        "1": [
+            { "date": moment("2015-01-01"), "type": 'withdrawal', "amount": -120.00, "payee": 'EVN', "note": 'Сметка за електричество'},
+            { "date": moment("2015-01-03"), "type": 'withdrawal', "amount": -24.00, "payee": 'CinemaCity', "note": 'Билети за кино'},
+            { "date": moment("2015-01-05"), "type": 'withdrawal', "amount": -20.00, "payee": 'EVN', "note": 'Обяд'},
+            { "date": moment("2015-01-07"), "type": 'deposit', "amount": 1200.00, "payee": 'Фирма ООД', "note": 'Заплата'},
+        ],
+        "2": [
+            { "date": moment("2015-01-01"), "type": 'withdrawal', "amount": -120.00, "payee": 'EVN', "note": 'Сметка за електричество'},
+            { "date": moment("2015-01-03"), "type": 'withdrawal', "amount": -24.00, "payee": 'CinemaCity', "note": 'Билети за кино'},
+            { "date": moment("2015-01-05"), "type": 'withdrawal', "amount": -20.00, "payee": 'EVN', "note": 'Обяд'},
+            { "date": moment("2015-01-07"), "type": 'deposit', "amount": 1200.00, "payee": 'Фирма ООД', "note": 'Заплата'},
+        ]
+    }
+};
