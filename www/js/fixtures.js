0d5a4862867670d59ffcf6bfed0d1bdafeabab90
Todor Mitovski <harvaru@gmail.com>
2015-02-18 16:38:35 +0200

    Clean up code

--- www/js/fixtures.js
+++ www/js/fixtures.js
@@ -1,6 +1,4 @@
 var transactions = {
-<<<<<<< HEAD
-=======
     2015: {
         1: [
             { "date": moment("2015-01-01", "YYYY-MM-DD"), "type": 'withdrawal', "amount": -120.00, "payee": 'EVN', "note": 'Сметка за електричество'},
@@ -13,7 +11,6 @@ var transactions = {
             { "date": moment("2015-01-03", "YYYY-MM-DD"), "type": 'withdrawal', "amount": -24.00, "payee": 'CinemaCity', "note": 'Билети за кино'},
             { "date": moment("2015-01-05", "YYYY-MM-DD"), "type": 'withdrawal', "amount": -20.00, "payee": 'EVN', "note": 'Обяд'},
             { "date": moment("2015-01-07", "YYYY-MM-DD"), "type": 'deposit', "amount": 1200.00, "payee": 'Фирма ООД', "note": 'Заплата'},
->>>>>>> upstream/master
         ]
     }
 };
