bc05e63dd7beeedfcbf1c06ad0be19bd19618daa
Atanas Vasilev <atanas.vasilev@gmail.com>
2015-02-19 02:12:16 +0200

    Populate a SELECT with options constisting of distinct months with existing transactions

--- www/js/handlers.js
+++ www/js/handlers.js
@@ -31,3 +31,40 @@ function newTransactionSubmitted() {
     // console.log(transaction);
     return false;
 }
+
+function arrayHasOwnIndex(array, prop) {
+    return array.hasOwnProperty(prop) && /^0$|^[1-9]\d*$/.test(prop) && prop <= 4294967294; // 2^32 - 2
+}
+
+function getDistinctMonths(transactions) {
+    var dMonths = [];
+
+    for (var year in transactions) {
+        if (arrayHasOwnIndex(transactions, year)) {
+            for (var month in transactions[year]) {
+                if (arrayHasOwnIndex(transactions[year], month)) {
+                    dMonths.push(String(year) + '/' + String(++month));
+                }
+            }
+        }
+    }
+    return dMonths;
+}
+
+function initTransactions() {
+    if(typeof(Storage) !== "undefined") {
+        // global vars
+        appTransactions = JSON.parse(sessionStorage.getItem('sessionTransactions')) || fixtureTransactions;
+    }
+
+    // global
+    distinctMonths = getDistinctMonths(appTransactions);
+
+    var monthSelect = document.getElementById("distinctMonthsSelect");
+
+    // populate the select with distinct months that actually have transactions
+    for (var i = 0,op = i+1; i < distinctMonths.length; i++,op++){
+        monthSelect[op] = new Option(distinctMonths[i],distinctMonths[i],false,false)
+    }
+
+}
