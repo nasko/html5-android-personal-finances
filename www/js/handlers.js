713011a2007127e9d7a99c3e3d5b103052a91eb9
Atanas Vasilev <atanas.vasilev@gmail.com>
2015-02-19 03:34:59 +0200

    Implemented queries: monthly balance and average monthly expenditure

--- www/js/handlers.js
+++ www/js/handlers.js
@@ -32,6 +32,14 @@ function newTransactionSubmitted() {
     return false;
 }
 
+function newQuerySubmitted(e) {
+
+    var queryMethod = querySubmitActor.id;
+    var result = window[queryMethod]();
+    //console.log(querySubmitActor);
+    return false;
+}
+
 function arrayHasOwnIndex(array, prop) {
     return array.hasOwnProperty(prop) && /^0$|^[1-9]\d*$/.test(prop) && prop <= 4294967294; // 2^32 - 2
 }
@@ -51,7 +59,7 @@ function getDistinctMonths(transactions) {
     return dMonths;
 }
 
-function initTransactions() {
+function initApp() {
     if(typeof(Storage) !== "undefined") {
         // global vars
         appTransactions = JSON.parse(sessionStorage.getItem('sessionTransactions')) || fixtureTransactions;
@@ -67,4 +75,74 @@ function initTransactions() {
         monthSelect[op] = new Option(distinctMonths[i],distinctMonths[i],false,false)
     }
 
+    addEvent(document.getElementById('queryAverageExpenditure'), 'click', queryButtonClicked);
+    addEvent(document.getElementById('queryMonthlyBalance'), 'click', queryButtonClicked);
+    addEvent(document.getElementById('queryTransactionsForm'), 'submit', newQuerySubmitted);
+    addEvent(document.getElementById('newTransactionForm'), 'submit', newTransactionSubmitted);
+
 }
+
+function queryButtonClicked(e) {
+    e = e || window.event;
+    // global var
+    querySubmitActor = e.target || e.srcElement;
+    return true;
+}
+
+function daysInMonth(y,m) {
+    m = parseInt(m) + 1;
+    return moment(String(y) + '-' + String(m), "YYYY-M").daysInMonth();
+}
+
+function queryMonthlyBalance() {
+    var monthSelected = document.getElementById("distinctMonthsSelect").value;
+    var parts = monthSelected.split('/');
+    var year = parts[0];
+    // months are zero-based
+    var month = parts[1]-1;
+
+    var transactions = appTransactions[year][month];
+
+    var monthlyBalance = 0.00;
+    var trLength = transactions.length;
+
+    for(var i= 0; i < trLength; i++) {
+        monthlyBalance += transactions[i].amount;
+    }
+
+    console.log("Month: " + monthSelected);
+    console.log("Monthly balance: " + monthlyBalance.toFixed(2));
+
+    return monthlyBalance.toFixed(2);
+}
+
+function queryAverageExpenditure() {
+    var monthSelected = document.getElementById("distinctMonthsSelect").value;
+    var parts = monthSelected.split('/');
+    var year = parts[0];
+    // months are zero-based
+    var month = parts[1]-1;
+
+    var transactions = appTransactions[year][month];
+    var dInMonth = daysInMonth(year, month);
+
+    var totalExpenditure = 0.00;
+    var trLength = transactions.length;
+
+    for(var i= 0; i < trLength; i++) {
+        var amount = transactions[i].amount;
+        // only withdrawals are regarded
+        if (0 > amount) {
+            totalExpenditure += amount;
+        }
+    }
+
+    var avgExp = (Math.abs(totalExpenditure) / dInMonth).toFixed(2);
+
+    console.log("Total expenditure: " + totalExpenditure);
+    console.log("Month: " + monthSelected);
+    console.log("Days in month: " + dInMonth);
+    console.log("Average expenditure: " + avgExp);
+    return avgExp;
+}
+
